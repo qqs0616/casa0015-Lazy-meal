a8cb109c98464ec9e917ce88c4918622c208c5b8
qqs0616 <113430856+qqs0616@users.noreply.github.com>
2023-04-24 15:13:59 +0100

    Add files via upload

---
+++ App/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/qqs0616/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/qqs0616/Desktop/Mobile/food_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/qqs0616/Desktop/Mobile/food_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/qqs0616/Desktop/Mobile/food_app/.dart_tool/package_config.json"
